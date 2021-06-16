7c8e4f22523e0d0dbb120e485eb6824d8c0ee563
Terrain2 <37938646+Terrain2@users.noreply.github.com>
2021-06-16 07:19:13 +0200

    fix missing scripts, fix player status text size

--- Assets/Scripts/Assembly-CSharp/DrawGrass.cs
+++ Assets/Scripts/Assembly-CSharp/DrawGrass.cs
@@ -5,288 +5,296 @@ using UnityEngine;
 public class DrawGrass : MonoBehaviour
 {
 
-	public void ClearMesh()
-	{
-		this.positions.Clear();
-		this.indicies.Clear();
-		this.normals.Clear();
-		this.length.Clear();
-	Destroy(this.mesh);
-		this.i = 0;
-		this.positions = new List<Vector3>();
-		this.indicies = new List<int>();
-		this.colors = new List<Color>();
-		this.normals = new List<Vector3>();
-		this.length = new List<Vector2>();
-	}
-
-
-	private void Awake()
-	{
-		if (!CurrentSettings.grass)
-		{
-		Destroy(this.grassObject.gameObject);
-		Destroy(base.gameObject);
-			return;
-		}
-		this.ClearMesh();
-		base.InvokeRepeating(nameof(SlowUpdate), 0f, this.updateRate);
-		this.currentPositions = new Dictionary<Vector3, bool>();
-	}
-
-
-	private void SlowUpdate()
-	{
-		if (!CurrentSettings.grass)
-		{
-		Destroy(this.grassObject.gameObject);
-		Destroy(base.gameObject);
-			return;
-		}
-		this.UpdateGrass();
-	}
-
-
-	private void UpdateGrass()
-	{
-		if (!this.target)
-		{
-			if (!PlayerMovement.Instance)
-			{
-				return;
-			}
-			this.target = PlayerMovement.Instance.playerCam;
-		}
-		Vector3 vector = this.posToGridPos(this.target.position);
-		if (this.currentGridPos != vector)
-		{
-			this.ClearMesh();
-			this.currentGridPos = vector;
-			float num = (float)5;
-			int num2 = 5;
-			int num3 = 0;
-			int num4 = Mathf.FloorToInt(num / 2f);
-			int num5 = num4 + 1;
-			int num6 = Mathf.FloorToInt((float)num2 / 2f);
-			int num7 = num6 + 1;
-			for (int i = -num4; i < num5; i++)
-			{
-				for (int j = -num4; j < num5; j++)
-				{
-					int d = 1;
-					if (i <= -num6 || i >= num7 || j <= -num6 || j >= num7)
-					{
-						d = 2;
-					}
-					Vector3 vector2 = this.currentGridPos + new Vector3((float)i, 0f, (float)j) * (float)this.gridSize;
-					this.CreateNewMesh(new Vector3((float)i, 0f, (float)j) * (float)this.gridSize, d);
-					num3++;
-					Debug.DrawLine(vector2, vector2 + Vector3.up * 50f, Color.red, 50f);
-				}
-			}
-			try
-			{
-				MonoBehaviour.print("setting grass mesh");
-				this.mesh = new Mesh();
-				this.mesh.SetVertices(this.positions);
-				this.indi = this.indicies.ToArray();
-				this.mesh.SetIndices(this.indi, MeshTopology.Points, 0);
-				this.mesh.SetUVs(0, this.length);
-				this.mesh.SetColors(this.colors);
-				this.mesh.SetNormals(this.normals);
-				this.filter.mesh = this.mesh;
-			}
-			catch
-			{
-				Debug.LogError("Failed to draw grass");
-			}
-		}
-	}
-
-
-	public Vector3 posToGridPos(Vector3 point)
-	{
-		float num = point.x;
-		float num2 = point.z;
-		if (num < 0f)
-		{
-			num -= (float)this.gridSize;
-		}
-		if (num2 < 0f)
-		{
-			num2 -= (float)this.gridSize;
-		}
-		float num3 = num - num % (float)this.gridSize;
-		float num4 = num2 - num2 % (float)this.gridSize;
-		float x = num3 + (float)this.gridSize / 2f;
-		num4 += (float)this.gridSize / 2f;
-		return new Vector3(x, 0f, num4);
-	}
+    public void ClearMesh()
+    {
+        this.positions.Clear();
+        this.indicies.Clear();
+        this.normals.Clear();
+        this.length.Clear();
+        try
+        {
+            Destroy(this.mesh);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError(ex);
+        }
+        this.mesh = null;
+        this.i = 0;
+        this.positions = new List<Vector3>();
+        this.indicies = new List<int>();
+        this.colors = new List<Color>();
+        this.normals = new List<Vector3>();
+        this.length = new List<Vector2>();
+    }
+
+
+    private void Awake()
+    {
+        if (!CurrentSettings.grass)
+        {
+            Destroy(this.grassObject.gameObject);
+            Destroy(base.gameObject);
+            return;
+        }
+        this.ClearMesh();
+        base.InvokeRepeating(nameof(SlowUpdate), 0f, this.updateRate);
+        this.currentPositions = new Dictionary<Vector3, bool>();
+    }
+
+
+    private void SlowUpdate()
+    {
+        if (!CurrentSettings.grass)
+        {
+            Destroy(this.grassObject.gameObject);
+            Destroy(base.gameObject);
+            return;
+        }
+        this.UpdateGrass();
+    }
+
+
+    private void UpdateGrass()
+    {
+        if (!this.target)
+        {
+            if (!PlayerMovement.Instance)
+            {
+                return;
+            }
+            this.target = PlayerMovement.Instance.playerCam;
+        }
+        Vector3 vector = this.posToGridPos(this.target.position);
+        if (this.currentGridPos != vector)
+        {
+            this.ClearMesh();
+            this.currentGridPos = vector;
+            float num = (float)5;
+            int num2 = 5;
+            int num3 = 0;
+            int num4 = Mathf.FloorToInt(num / 2f);
+            int num5 = num4 + 1;
+            int num6 = Mathf.FloorToInt((float)num2 / 2f);
+            int num7 = num6 + 1;
+            for (int i = -num4; i < num5; i++)
+            {
+                for (int j = -num4; j < num5; j++)
+                {
+                    int d = 1;
+                    if (i <= -num6 || i >= num7 || j <= -num6 || j >= num7)
+                    {
+                        d = 2;
+                    }
+                    Vector3 vector2 = this.currentGridPos + new Vector3((float)i, 0f, (float)j) * (float)this.gridSize;
+                    this.CreateNewMesh(new Vector3((float)i, 0f, (float)j) * (float)this.gridSize, d);
+                    num3++;
+                    Debug.DrawLine(vector2, vector2 + Vector3.up * 50f, Color.red, 50f);
+                }
+            }
+            try
+            {
+                MonoBehaviour.print("setting grass mesh");
+                this.mesh = new Mesh();
+                this.mesh.SetVertices(this.positions);
+                this.indi = this.indicies.ToArray();
+                this.mesh.SetIndices(this.indi, MeshTopology.Points, 0);
+                this.mesh.SetUVs(0, this.length);
+                this.mesh.SetColors(this.colors);
+                this.mesh.SetNormals(this.normals);
+                this.filter.mesh = this.mesh;
+            }
+            catch
+            {
+                Debug.LogError("Failed to draw grass");
+            }
+        }
+    }
 
 
-	private void CreateNewMesh(Vector3 offset, int d)
-	{
-		if (PlayerMovement.Instance)
-		{
-			base.transform.position = PlayerMovement.Instance.transform.position;
-		}
-		float num = this.chunkDensity / (float)d;
-		float num2 = this.chunkLength / num;
-		int num3 = 0;
-		while ((float)num3 < num)
-		{
-			int num4 = 0;
-			while ((float)num4 < num)
-			{
-				float num5 = this.currentGridPos.x + offset.x - this.currentGridPos.x % num2;
-				float num6 = this.currentGridPos.z + offset.z - this.currentGridPos.z % num2;
-				float x = num5 + ((float)num3 - num / 2f) / num * this.chunkLength;
-				float z = num6 + ((float)num4 - num / 2f) / num * this.chunkLength;
-				Vector3 origin = new Vector3(x, base.transform.position.y + 50f, z);
-				RaycastHit raycastHit;
-				if (Physics.Raycast(new Ray(origin, Vector3.down), out raycastHit, 200f, this.hitMask.value) && this.i < this.grassLimit && raycastHit.normal.y <= 1f + this.normalLimit && raycastHit.normal.y >= 1f - this.normalLimit && WorldUtility.WorldHeightToBiome(raycastHit.point.y) == TextureData.TerrainType.Grass && raycastHit.collider.gameObject.CompareTag("Terrain"))
-				{
-					this.hitPos = raycastHit.point;
-					this.hitNormal = raycastHit.normal;
-					this.hitPos -= this.grassObject.transform.position;
-					this.positions.Add(this.hitPos);
-					this.indicies.Add(this.i);
-					this.length.Add(new Vector2(this.sizeWidth, this.sizeLength));
-					this.colors.Add(new Color(this.AdjustedColor.r + Random.Range(0f, 1f) * this.rangeR, this.AdjustedColor.g + Random.Range(0f, 1f) * this.rangeG, this.AdjustedColor.b + Random.Range(0f, 1f) * this.rangeB, 1f));
-					this.normals.Add(raycastHit.normal);
-					this.i++;
-				}
-				num4++;
-			}
-			num3++;
-		}
-	}
+    public Vector3 posToGridPos(Vector3 point)
+    {
+        float num = point.x;
+        float num2 = point.z;
+        if (num < 0f)
+        {
+            num -= (float)this.gridSize;
+        }
+        if (num2 < 0f)
+        {
+            num2 -= (float)this.gridSize;
+        }
+        float num3 = num - num % (float)this.gridSize;
+        float num4 = num2 - num2 % (float)this.gridSize;
+        float x = num3 + (float)this.gridSize / 2f;
+        num4 += (float)this.gridSize / 2f;
+        return new Vector3(x, 0f, num4);
+    }
 
 
-	public Mesh mesh;
+    private void CreateNewMesh(Vector3 offset, int d)
+    {
+        if (PlayerMovement.Instance)
+        {
+            base.transform.position = PlayerMovement.Instance.transform.position;
+        }
+        float num = this.chunkDensity / (float)d;
+        float num2 = this.chunkLength / num;
+        int num3 = 0;
+        while ((float)num3 < num)
+        {
+            int num4 = 0;
+            while ((float)num4 < num)
+            {
+                float num5 = this.currentGridPos.x + offset.x - this.currentGridPos.x % num2;
+                float num6 = this.currentGridPos.z + offset.z - this.currentGridPos.z % num2;
+                float x = num5 + ((float)num3 - num / 2f) / num * this.chunkLength;
+                float z = num6 + ((float)num4 - num / 2f) / num * this.chunkLength;
+                Vector3 origin = new Vector3(x, base.transform.position.y + 50f, z);
+                RaycastHit raycastHit;
+                if (Physics.Raycast(new Ray(origin, Vector3.down), out raycastHit, 200f, this.hitMask.value) && this.i < this.grassLimit && raycastHit.normal.y <= 1f + this.normalLimit && raycastHit.normal.y >= 1f - this.normalLimit && WorldUtility.WorldHeightToBiome(raycastHit.point.y) == TextureData.TerrainType.Grass && raycastHit.collider.gameObject.CompareTag("Terrain"))
+                {
+                    this.hitPos = raycastHit.point;
+                    this.hitNormal = raycastHit.normal;
+                    this.hitPos -= this.grassObject.transform.position;
+                    this.positions.Add(this.hitPos);
+                    this.indicies.Add(this.i);
+                    this.length.Add(new Vector2(this.sizeWidth, this.sizeLength));
+                    this.colors.Add(new Color(this.AdjustedColor.r + Random.Range(0f, 1f) * this.rangeR, this.AdjustedColor.g + Random.Range(0f, 1f) * this.rangeG, this.AdjustedColor.b + Random.Range(0f, 1f) * this.rangeB, 1f));
+                    this.normals.Add(raycastHit.normal);
+                    this.i++;
+                }
+                num4++;
+            }
+            num3++;
+        }
+    }
 
 
-	public MeshFilter filter;
+    public Mesh mesh;
 
 
-	public Color AdjustedColor;
+    public MeshFilter filter;
 
 
-	[Range(1f, 600000f)]
-	public int grassLimit = 50000;
+    public Color AdjustedColor;
 
 
-	private Vector3 lastPosition = Vector3.zero;
+    [Range(1f, 600000f)]
+    public int grassLimit = 50000;
 
 
-	public int toolbarInt;
+    private Vector3 lastPosition = Vector3.zero;
 
 
-	[SerializeField]
-	private List<Vector3> positions = new List<Vector3>();
+    public int toolbarInt;
 
 
-	[SerializeField]
-	private List<Color> colors = new List<Color>();
+    [SerializeField]
+    private List<Vector3> positions = new List<Vector3>();
 
 
-	[SerializeField]
-	private List<int> indicies = new List<int>();
+    [SerializeField]
+    private List<Color> colors = new List<Color>();
 
 
-	[SerializeField]
-	private List<Vector3> normals = new List<Vector3>();
+    [SerializeField]
+    private List<int> indicies = new List<int>();
 
 
-	[SerializeField]
-	private List<Vector2> length = new List<Vector2>();
+    [SerializeField]
+    private List<Vector3> normals = new List<Vector3>();
 
 
-	public bool painting;
+    [SerializeField]
+    private List<Vector2> length = new List<Vector2>();
 
 
-	public bool removing;
+    public bool painting;
 
 
-	public bool editing;
+    public bool removing;
 
 
-	public int i;
+    public bool editing;
 
 
-	public float sizeWidth = 1f;
+    public int i;
 
 
-	public float sizeLength = 1f;
+    public float sizeWidth = 1f;
 
 
-	public float density = 1f;
+    public float sizeLength = 1f;
 
 
-	public float normalLimit = 1f;
+    public float density = 1f;
 
 
-	public float rangeR;
+    public float normalLimit = 1f;
 
 
-	public float rangeG;
+    public float rangeR;
 
 
-	public float rangeB;
+    public float rangeG;
 
 
-	public LayerMask hitMask = 1;
+    public float rangeB;
 
 
-	public LayerMask paintMask = 1;
+    public LayerMask hitMask = 1;
 
 
-	public float brushSize;
+    public LayerMask paintMask = 1;
 
 
-	private Vector3 mousePos;
+    public float brushSize;
 
 
-	[HideInInspector]
-	public Vector3 hitPosGizmo;
+    private Vector3 mousePos;
 
 
-	private Vector3 hitPos;
+    [HideInInspector]
+    public Vector3 hitPosGizmo;
 
 
-	[HideInInspector]
-	public Vector3 hitNormal;
+    private Vector3 hitPos;
 
 
-	private int[] indi;
+    [HideInInspector]
+    public Vector3 hitNormal;
 
 
-	private float updateRate = 0.5f;
+    private int[] indi;
 
 
-	public Transform grassObject;
+    private float updateRate = 0.5f;
 
 
-	public float chunkLength = 20f;
+    public Transform grassObject;
 
 
-	public float chunkDensity = 10f;
+    public float chunkLength = 20f;
 
 
-	public int nChunks = 16;
+    public float chunkDensity = 10f;
 
 
-	public int iterations;
+    public int nChunks = 16;
 
 
-	private Dictionary<Vector3, bool> currentPositions;
+    public int iterations;
 
 
-	public Transform target;
+    private Dictionary<Vector3, bool> currentPositions;
 
 
-	private Vector3 currentGridPos;
+    public Transform target;
 
 
-	public int gridSize = 20;
+    private Vector3 currentGridPos;
+
+
+    public int gridSize = 20;
 }
