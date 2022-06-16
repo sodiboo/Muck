734864f683f1056a4c35fcec8bd9cd15d3aa3216
Terrain2 <37938646+Terrain2@users.noreply.github.com>
2022-06-17 01:59:30 +0200

    powerups icons too

--- docs/powerup.ts
+++ docs/powerup.ts
@@ -52,7 +52,7 @@ function info(powerup: Powerup): string {
     `###### ![${fragment(powerup.name)}]`,
   );
   references.push(
-    `[${fragment(powerup.name)}]: ../Assets/Texture2D/${
+    `[${fragment(powerup.name)}]: https://raw.githubusercontent.com/Terrain2/Muck/main/Assets/Texture2D/${
       encodeURIComponent(images.get(powerup.sprite)!)
     }`,
   );
